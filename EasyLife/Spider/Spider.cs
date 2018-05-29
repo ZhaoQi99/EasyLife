46d93782fed772fa4821b22518c61d6400abfdaa
QiZhao <956361916@qq.com>
2018-05-29 20:52:54 +0800

    Add SpiderNotice

---
+++ EasyLife/Spider/Spider.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyLife.Spider
+{
+    class Spider
+    {
+        public string url { set; get; } = string.Empty;//要爬取的网页的url
+        public Encoding encode { set; get; } = Encoding.UTF8;//目标url的网页编码格式
+
+        private WebRequest request;
+        private WebResponse response;
+        public Spider(string url)
+        {
+            this.url = url;
+            request = WebRequest.Create(url);
+            request.Credentials = CredentialCache.DefaultCredentials;
+        }
+
+        //获取目标url的Html代码
+        public string getHtml()
+        {
+            string reader=string.Empty;
+            try
+            {
+                response = request.GetResponse();
+                reader = new StreamReader(response.GetResponseStream(), encode).ReadToEnd();
+            }
+            catch
+            {
+                //Todo:Hanle Exception
+            }
+            return reader;
+        }
+        //关闭response响应流
+        public void closeResponse()
+        {
+            try
+            {
+                response.Close();
+            }
+            catch (Exception e)
+            {
+                //Todo: handle Exception
+            }
+        }
+    }
+}
---
+++ EasyLife/Spider/SpiderNotice.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyLife.Spider;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace EasyLife.Spider
+{
+    class SpiderNotice : Spider
+    {
+        private string url_main;//各个通知链接的公共部分
+        private string subject;//发送邮件的标题
+        private string pattern;//
+        private ArrayList all_notice = new ArrayList();
+        public string parse { get; set; } = string.Empty;
+        public SpiderNotice(string url, string pattern, string url_main,string subject) : base(url)
+        {
+            this.subject = subject;
+            this.pattern = pattern;
+            this.url_main = url_main;
+            this.parse = "yyyy-MM-dd";
+        }
+        public void getNotice()
+        {
+            all_notice.Clear();
+            Regex r = new Regex(pattern, RegexOptions.ExplicitCapture);
+            MatchCollection mc = r.Matches(getHtml());
+            foreach (Match m in mc)
+            {
+                GroupCollection group = m.Groups;
+                DateTime date = DateTime.ParseExact(group["date"].Value, parse, System.Globalization.CultureInfo.InstalledUICulture);
+                string link = url_main + group["link"].Value;
+                Notice n = new Notice(group["title"].Value, link, date);
+                all_notice.Add(n);
+            }
+        }
+        public Notice index(int index)
+        {
+            if (index >= all_notice.Count)
+                return null;
+            else
+                return all_notice[index] as Notice;
+        }
+    }
+}
